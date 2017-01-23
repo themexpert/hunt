59d9e11d8c4829d1d96751fd8ac40b1e34d55c48
Khandaker Ikrama <ikrama.213@gmail.com>
2017-01-23 12:45:01 +0600

    list page done

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.modal').modal();
+        $('select').material_select();
+        $('.collapsible').collapsible();
+
+    });
+})(jQuery);
