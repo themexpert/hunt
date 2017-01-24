e680cf7ff450749de949ede95d9d5ebea4d8c362
Iftekher Sunny <iftekhersunny@hotmail.com>
2017-01-24 11:41:47 +0600

    Added passport client component

--- resources/views/settings/token.blade.php
+++ resources/views/settings/token.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
             <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
