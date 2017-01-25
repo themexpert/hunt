056801366fa16dfdb37938ab2a299919fa077a90
Khandaker Ikrama <ikrama.213@gmail.com>
2017-01-25 11:56:24 +0600

    Preloader added. and jquery version change...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,5 +7,13 @@
         $('.collapsible').collapsible();
         // $('.tooltipped').tooltip({delay: 50});
 
+        //Preloader
+        $(window).load(function () {
+            $('.preloader-box').fadeOut('slow', function () {
+                $(this).remove();
+            });
+        });
+
     });
 })(jQuery);
+
