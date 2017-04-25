2910350f502c74dd3d403d233c7ff635d9478f17
limon <ikrama.213@gmail.com>
2017-04-25 11:11:39 +0600

    Settings page markup done.

--- resources/assets/js/components/settings/settings.vue
+++ resources/assets/js/components/settings/settings.vue
@@ -22,44 +22,68 @@
                   <a href="#">Sign Out</a>
                 </div><!--/.user-info-->
 
-                <hr>
-
-                <div class="general-settings mt30">
-                  <h5>General Settings</h5>
-
-
-                </div><!--/.general-settings-->
-
-                <div class="profile-settings mt30">
-                  <h5>Profile Settings</h5>
-                </div><!--/.profile-info-->
-
-
-            </div>
+            </div><!--/.col-->
 
             <div class="col s9">
                 <div class="card mt0">
                     <div class="card-content settings-box">
-                      <h5 class="mt0">General Settings</h5>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                      <div class="general-settings">
 
-                      <div class="logo-change mt30">
-                        <div class="file-field input-field">
-                          <div class="btn">
-                            <span>Upload Logo</span>
-                            <input type="file">
-                          </div>
-                          <div class="file-path-wrapper">
-                            <input class="file-path validate" type="text" placeholder="Upload your logo">
-                          </div>
-                        </div>
-                      </div><!--/.logo-change-->
+                        <h5 class="mt0">General Settings</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-                      <div class="profile-setting">
+                        <div class="logo-change mt30">
+                          <div class="file-field input-field">
+                            <div class="btn">
+                              <span>Upload Logo</span>
+                              <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                              <input class="file-path validate" type="text" placeholder="Upload your logo">
+                            </div>
+                          </div>
+                        </div><!--/.logo-change-->
+
+                        <div class="favicon-change mt30">
+                          <div class="file-field input-field">
+                            <div class="btn">
+                              <span>Upload Favicon</span>
+                              <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                              <input class="file-path validate" type="text" placeholder="Upload your favicon">
+                            </div>
+                          </div>
+                        </div><!--/.favicon-change-->
+
+                        <div class="mt30">
+                          <div class="input-field">
+                            <input id="company_name" type="text" value="ThemeXpert">
+                            <label class="active" for="company_name">Company Name</label>
+                          </div>
+                        </div><!--/.mt30-->
+
+                      </div><!--/.general-settings-->
+
+                      <div class="profile-setting mt30">
                         <h5>Profile Settings</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-                      </div>
+                        <div class="mt30">
+                          <div class="input-field">
+                            <input id="change_name" type="text" value="Khandaker Ikrama">
+                            <label class="active" for="change_name">Change Name</label>
+                          </div>
+                        </div><!--/.mt30-->
+
+                        <div class="mt30">
+                          <div class="input-field">
+                            <input id="change_password" type="password" value="12345">
+                            <label class="active" for="change_password">Change Password</label>
+                          </div>
+                        </div><!--/.mt30-->
+
+                      </div><!--/.profile-settings-->
                     </div><!--settings-box-->
                 </div><!--/.feature-req-->
             </div>
